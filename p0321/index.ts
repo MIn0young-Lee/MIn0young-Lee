a84a6ffbc7dfeb1cc9147ecf5f1df8fee0ac94d6
min0young-lee <min0ee@sizl.co.kr>
2025-03-21 09:47:22 +0900

    0321 실습시작

---
+++ p0321/index.ts
@@ -0,0 +1,25 @@
+class Calculator {
+    add(a, b) {
+        return a + b;
+    }
+    subtract(a, b) {
+        return a - b;
+    }
+    multiply(a, b) {
+        return a * b;
+    }
+    divide(a, b) {
+        if (b === 0) {
+            return "Cannot divide by zero";
+        }
+        else {
+            return a / b;
+        }
+    }
+}
+const calculator = new Calculator();
+console.log("Add: ", calculator.add(10, 5));
+console.log("Substract: ", calculator.subtract(18, 2));
+console.log("Multiply: ", calculator.multiply(12, 12 ));
+console.log("Divide: ", calculator.divide(20, 4));
+console.log(calculator.divide(20, 0));
